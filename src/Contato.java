d896cfc5b840f27eea9ac90eb14640996e6bcf3b
Yago Gomes <yaguamineral@gmail.com>
2023-09-18 21:35:49 -0300

    Criação do repositorio

---
+++ src/Contato.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Contato {
+    String nome;
+    String telefone;
+    String nascimento;
+    String email;
+    String celular;
+
+    public Contato(String nome, String telefone, String nascimento, String email, String celular) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.nascimento = nascimento;
+        this.email = email;
+        this.celular = celular;
+    }
+}
+
